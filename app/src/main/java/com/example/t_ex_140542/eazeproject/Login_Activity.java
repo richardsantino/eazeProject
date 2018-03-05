b5442f87872381599979feee0a327f7aa9f22c52
Richard Santino <richard_one@outlook.com>
2018-03-05 12:27:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/t_ex_140542/eazeproject/Login_Activity.java
@@ -0,0 +1,17 @@
+package com.example.t_ex_140542.eazeproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+
+    public void LoginClick(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/t_ex_140542/eazeproject/Registration_Activity.java
@@ -0,0 +1,17 @@
+package com.example.t_ex_140542.eazeproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registration_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_layout);
+    }
+
+    public void click(View view) {
+    }
+}
